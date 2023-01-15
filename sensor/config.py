aeb7841ba3f32d4d8d9b36bd28fd696c9e1cf7fa
BabuJ21 <babujayaraman21@gmail.com>
2023-01-16 02:44:38 +0530

    data ingestion completed and successful

---
+++ sensor/config.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+import pymongo
+import json
+import os, sys
+from dataclasses import dataclass
+
+@dataclass
+class EnvironmentVariable:
+    mongo_db_url:str = os.getenv("MONGO_DB_URL")
+    aws_access_key_id:str = os.getenv("AWS_ACCESS_KEY_ID")
+    aws_secret_access_key:str = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+## Provide the mongodb localhost url to connect python to mongodb
+env_var = EnvironmentVariable()
+mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
